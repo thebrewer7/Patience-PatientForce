304acfde5f3a7ed5b2de387c9e1307b315816401
Andrew Duffey <andrewduffey@outlook.com>
2018-07-29 18:25:30 -0500

    review

--- Project2/Patience/src/app/components/profile-page/profile-page.component.ts
+++ Project2/Patience/src/app/components/profile-page/profile-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Review } from '../../objects/review';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  reviews: Review[] = [{id: 1, rating: 3, comment: 'dsfsdfsdf fdsdfsdf'}];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  ratingToStars(rating: number) {
+    let stars = '';
+    for (let i = 0; i < 5; ++i) {
+      if (i < rating) {
+        stars = stars + '\u2605';
+      } else {
+        stars = stars + '\u2606';
+      }
+    }
+    return stars;
+  }
+
 
 }
