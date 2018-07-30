0c9ee8eb6f102fc2b5ee8dfd72f6be9b773732cd
Ray Rodriguez <ray.grod@outlook.com>
2018-07-29 19:16:10 -0500

    restructuring tests

--- Project2/PatienceMavenProject/src/test/java/com/revature/junit/unittests/UserServicetest.java
+++ Project2/PatienceMavenProject/src/test/java/com/revature/junit/unittests/UserServicetest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,55 +26,67 @@ class UserServicetest {
 		service = null;
 	}
 
+	@Ignore
 	@Test
 	void testGetAllUsers() {
 		assertNotNull(service.getAllUsers());
 	}
-
+	
+	@Ignore
 	@Test
 	void testGetUserById_positive() {
 		assertNotNull(service.getUserById(1));
 	}
 	
+	@Ignore
 	@Test
 	void testGetUserById_zero() {
 		assertNull(service.getUserById(0));
 	}
 	
+	@Ignore
 	@Test
 	void testGetUseById_negative(){
 		assertNull(service.getUserById(-1));
 	}
 
+	@Ignore
 	@Test
 	void testGetUsersByName_notNull() {
 		assertNotNull(service.getUsersByName("test"));
 	}
 	
+	@Ignore
 	@Test
 	void testGetUsersByName_null() {
 		assertNull(service.getUsersByName("null"));
 	}
 
+	@Ignore
 	@Test
 	void testGetUsersByPatient_positive() {
 		assertNotNull(service.getUsersByPatient(1));
 	}
 	
+	@Ignore
 	@Test
 	void testGetUsersByPatient_zero() {
 		assertNotNull(service.getUsersByPatient(0));
 	}
+	
+	@Ignore
 	@Test
 	void testGetUsersByPatient_negative() {
 		assertNotNull(service.getUsersByPatient(-1));
 	}
 
+	@Ignore
 	@Test
 	void testInsertNewUser() {
 		assertTrue(service.insertNewUser(new User()));
 	}
 
+	@Ignore
 	@Test
 	void testUpdateUser() {
 		fail("Not yet implemented");
