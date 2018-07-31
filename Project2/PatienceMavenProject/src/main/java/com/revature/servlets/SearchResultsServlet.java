ab627dc1a03b104111fd1513e6633284d30f754f
Austin Davis <audavis12@gmail.com>
2018-07-30 20:05:16 -0500

    Near-EOD

--- Project2/PatienceMavenProject/src/main/java/com/revature/servlets/SearchResultsServlet.java
+++ Project2/PatienceMavenProject/src/main/java/com/revature/servlets/SearchResultsServlet.java
@@ -2,14 +2,11 @@ package com.revature.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.revature.services.UserService;
 
 /**
  * Servlet implementation class SearchResultsServlet
