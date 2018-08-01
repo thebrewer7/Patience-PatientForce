c6cf44d7a1a7d3e50eb10d9fb33fcefcaa2e3c49
Logan Brewer <logan211@live.com>
2018-08-01 17:39:44 -0500

    populate the correct people from their lists

--- Project2/Patience/src/app/components/doctor-page/doctor-page.component.ts
+++ Project2/Patience/src/app/components/doctor-page/doctor-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../objects/patient';
 
+import { DOCTORS } from '../../mock-doctors';
 import { PATIENTS } from '../../mock-patients';
 
 @Component({
@@ -10,11 +11,136 @@ import { PATIENTS } from '../../mock-patients';
 })
 export class DoctorPageComponent implements OnInit {
 
+  doctors = DOCTORS;
   patients = PATIENTS;
+  doctorsPatients = []
 
   constructor() { }
 
   ngOnInit() {
+    var activeuserid = 1;
+      switch(activeuserid){
+        case 1:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 1 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 2:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 2 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 3:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 3 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 4:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 4 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 5:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 5 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 6:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 6 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.patients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 7:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 7 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 8:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 8 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 9:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 9 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+        case 10:
+          for ( var i = 0; i < this.doctors.length; i++ )
+          {
+            if ( this.doctors[i]['id'] == 10 )
+            {
+              for ( var j = 0; j < this.doctors[i]['patients'].length; j++ )
+              {
+                this.doctorsPatients[j] = this.doctors[i]['patients'][j];
+              }
+            }
+          }
+          break;
+      }
   }
 
 }
