8fa5d1a3f54f085d058ceec0ecda1afa48a126e2
Andrew Duffey <andrewduffey@outlook.com>
2018-08-03 13:27:40 -0500

    fixed quick search

--- Project2/Patience/src/app/components/quick-lookup/quick-lookup.component.ts
+++ Project2/Patience/src/app/components/quick-lookup/quick-lookup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../../services/patient/patient.service';
+import { Patient } from '../../objects/patient';
 
 @Component({
   selector: 'app-quick-lookup',
@@ -9,6 +10,7 @@ import { PatientService } from '../../services/patient/patient.service';
 export class QuickLookupComponent implements OnInit {
 
   username = '';
+  patient: Patient;
 
   constructor(private patientService: PatientService) { }
 
@@ -19,7 +21,8 @@ export class QuickLookupComponent implements OnInit {
     // search for the patient
     console.log('QuickLookupComponent: search()');
     this.patientService.search(this.username).subscribe(
-      PASS => { console.log(PASS); },
+      PASS => { this.patient = PASS;
+        console.log(PASS); },
       FAIL => { console.log(FAIL); }
     );
   }
