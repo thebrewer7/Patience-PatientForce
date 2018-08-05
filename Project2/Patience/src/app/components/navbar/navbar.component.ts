9f267568441cbdef9db6c200c64a152692c8ae46
Logan Brewer <logan211@live.com>
2018-08-05 15:59:43 -0500

    fixing dropdown menu, checks if logged in when you click the menu div

--- Project2/Patience/src/app/components/navbar/navbar.component.ts
+++ Project2/Patience/src/app/components/navbar/navbar.component.ts
@@ -30,6 +30,16 @@ export class NavbarComponent implements OnInit {
     return localStorage.getItem('username');
   }
 
+  checkIfLoggedIn()
+  {
+    if ( this.getUsername() != null )
+    {
+      this.dropdownDisabled = false;
+      return;
+    }
+    this.dropdownDisabled = true;
+  }
+
   public fetchSearchFill() {
     // this.searchData = [{id:50, name:"logan", role:"doctor", certifications:[], degrees:[], experience:"sdjflksdjf slfjdslfj", rating:3, reviews:[{id:1, name:"logan", datePosted:"03-AUG-18 12.00.00 AM", rating:3, review:"sjfldjfs sljfdljfs"}], department:"The Department", patients:[]},
     //                   {id:51, name:"andrew", role:"doctor", certifications:[], degrees:[], experience:"sdjflksdjf slfjdslfj", rating:3, reviews:[{id:2, name:"logan", datePosted:"02-AUG-18 12.00.00 AM", rating:2, review:"sjfldjfs sljfdljfs"}], department:"The Department", patients:[]}];
