d70c71209e90ffaac3d1832bb68fb34a25776377
Logan Brewer <logan211@live.com>
2018-08-04 21:47:33 -0500

    trying to fix allHistories

--- Project2/Patience/src/app/components/patient-page/patient-page.component.ts
+++ Project2/Patience/src/app/components/patient-page/patient-page.component.ts
@@ -38,7 +38,10 @@ export class PatientPageComponent implements OnInit {
         console.log("===================datainside allHistories==================");
         for ( var d in data )
         {
-          this.allHistories[d] = data[d];
+          this.allHistories[d]['age'] = data[d]['age'];
+          this.allHistories[d]['weight'] = data[d]['weight'];
+          this.allHistories[d]['height'] = data[d]['height'];
+          this.allHistories[d]['bloodPressureCondition'] = data[d]['bloodPressureCondition'];
         }
         
         console.log(this.allHistories);
